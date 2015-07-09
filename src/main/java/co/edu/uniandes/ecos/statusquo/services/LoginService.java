2aa39a8b9fb8c1ff950ced0450b7abb4100af525
Dev <Dev@Dev-PC>
2015-07-09 04:36:14 -0500

    Commit shiro basico usuarios DB local

---
+++ src/main/java/co/edu/uniandes/ecos/statusquo/services/LoginService.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.ecos.statusquo.services;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.apache.shiro.subject.Subject;
+
+
+/**
+ *
+ * @author Dev
+ */
+@Path("/login")
+public class LoginService {
+
+    Realm realm = new JdbcRealm();
+    DefaultSecurityManager sm = new DefaultSecurityManager(realm);
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response userAuth(String username,String password,boolean remember) {
+        Response r = null;
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        token.setRememberMe(remember);
+        SecurityUtils.setSecurityManager(sm);
+        Subject currentUser = SecurityUtils.getSubject();
+        try {
+            currentUser.login(token);
+            r = Response.ok().entity(token).build();
+        } catch (IncorrectCredentialsException ice) {
+            System.out.println("Incorrect username/password!");
+        }
+
+        return r;
+    }
+}
